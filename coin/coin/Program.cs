5507c4f3e1fc8d91e6bac7d42587a57b474fde6b
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-03-05 19:39:21 +0530

    Added : Basic Program

---
+++ coin/coin/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int Coin_Head = 1; //Constant
+Random random = new Random(); //Computation // to create object
+int coinCheck = random.Next(0, 2); // to call the object
+if (coinCheck == Coin_Head) // to check condition 
+{
+    Console.WriteLine("Coin is Head");
+}
+else
+{
+    Console.WriteLine("Coin is Tails");
+}
---
+++ leap_year/leap_year/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Enter Year :"); // to take input from user
+int Year = int.Parse(Console.ReadLine()); // to pass the input and dubble check
+
+//method 1
+//if(((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)) // to check the comdition
+//{
+//    Console.WriteLine("{0} is a leap Year", Year);
+//}
+//else
+//{
+//    Console.WriteLine("{0} is not a leap Year", Year);
+//}
+
+// method 2
+if(DateTime.IsLeapYear(Year))
+{
+    Console.WriteLine("This is a leap year");
+}
+else
+{
+    Console.WriteLine("This is not a leap year");
+}
